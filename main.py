65446c07df7c2dd99e1ecd3d3243a857ceb957b7
qope <toqe159@gmail.com>
2019-03-27 02:12:18 +0900

    さっきのものと同じ．　add

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import csv
 from os import system
+from pydub import AudioSegment
 def download_mp3(num, word):
-    system('curl -o words/{}_{}.mp3 https://ssl.gstatic.com/dictionary/static/sounds/oxford/{}--_gb_1.mp3'.format(str(num).zfill(4),word,word))
+    system('curl -o pre_words/{}.mp3 https://ssl.gstatic.com/dictionary/static/sounds/oxford/{}--_gb_1.mp3'.format(word,word))
+    sound1 = AudioSegment.from_file("pre_words/{}.mp3".format(word), "mp3")
+    sound2 = AudioSegment.from_file("Silent3sec.mp3", "mp3")
+    sound = sound1+sound2
+    sound.export("words/{}_{}.mp3".format(num,word), format="mp3")
 def get_list():
     words = []
     with open('list.csv', 'r') as f:
@@ -10,9 +15,7 @@ def get_list():
         for row in reader:
             words.append(row[0])
     return words
-def add_silence(i):
-    system('cp Silent3sec.mp3 words/{}.mp3'.format(str(i).zfill(4)))
+
 words = get_list()
 for i, word in enumerate(words):
-    download_mp3(i*2, word)
-    add_silence(i*2+1)
+    download_mp3(i, word)
