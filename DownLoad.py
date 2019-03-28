31621f29dc06ac1a2054321d8854577c9d670bd3
qope <toqe159@gmail.com>
2019-03-28 21:29:46 +0900

    add

--- DownLoad.py
+++ DownLoad.py
@@ -1,32 +1,6 @@
-import csv
 from os import system
 from pydub import AudioSegment
 import MakeAudio
-
-
-
-class OneWord:
-
-    def __init__(self):
-        self.Url = "https://s3.amazonaws.com/audio.oxforddictionaries.com/en/mp3/pronunciation_gb_1_8.mp3"
-        self.Word = "pronunciation"
-        self.Index = 0
-        self.Audio = None
-        self.AudioTempPath = ""
-        self.AudioExportPath = ""
-        self.HaveE = False
-
-class OneWord2:
-
-    def __init__(self):
-        self.Url = "https://s3.amazonaws.com/audio.oxforddictionaries.com/en/mp3/apple_gb_1.mp3"
-        self.Word = "apple"
-        self.Index = 1
-        self.Audio = None
-        self.AudioTempPath = ""
-        self.AudioExportPath = ""
-        self.HaveE = False
-
 
 class DownLoad:
     def _download_mp3(self,word):
@@ -37,17 +11,11 @@ class DownLoad:
         except:
             word.HaveE = True
             print("error {}".format(word.Word))
-            
-            
-
     def Download(self,words:list):
         system("mkdir -p pre_words")
         for word in words:
             if word.HaveE == False:
                 self._download_mp3(word)
-                
-
-
 
 if __name__ == "__main__":
     wos = [OneWord(),OneWord2()]
--- main.py
+++ main.py
@@ -2,60 +2,39 @@ import ReadExcel
 import UrlFetcher
 import DownLoad
 import MakeAudio
-
-
 # _read :ReadExcel.ReadExcel
 # _urlf :UrlFetcher.UrlFetcher
 # _dl :DownLoad.DownLoad
 # _audio :MakeAudio.MakeAudio
-
-
-
-
 class OneWord:
 
     def __init__(self):
         self.Url = ""
         self.Word = ""
         self.Index = 0
-        
         self.AudioTempPath = ""
         self.AudioExportPath = ""
         self.HaveE = False
 
-
 class Main:
 
-    _words = []
-
     def _initWordsList(self,number):
+        self._words = []
         for i in range(number):
             self._words.append(OneWord())
 
-
- 
-
-
-
     def __init__(self):
-        
-
-        self._read = ReadExcel.ReadExcel(3)
+        self._read = ReadExcel.ReadExcel(1)
         self._urlf = UrlFetcher.UrlFetcher()
         self._dl = DownLoad.DownLoad()
         self._audio = MakeAudio.MakeAudio()
-
-        number :int = self._read.GetNumber()
+        number = self._read.GetNumber()
         self._initWordsList(number)
         self._read.SetWords(self._words)
         self._urlf.SetUrl(self._words)
-
-
-
         self._dl.Download(self._words)
         self._audio.MakeAudios(self._words)
-        self._audio.ConnectAudio()
-
+        self._audio.ConnectAudio2(30)
 
 
 if __name__ == "__main__":
